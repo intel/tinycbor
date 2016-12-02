976fd45a293f4152fdc8688549d60a81313fce2b
Thiago Macieira <thiago.macieira@intel.com>
2016-12-02 09:19:45 -0800

    Add 0.4 documentation.
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

---
+++ 0.4/menudata.js
@@ -0,0 +1,23 @@
+var menudata={children:[
+{text:'Main Page',url:'index.html'},
+{text:'Modules',url:'modules.html'},
+{text:'Data Structures',url:'annotated.html',children:[
+{text:'Data Structures',url:'annotated.html'},
+{text:'Data Structure Index',url:'classes.html'}]},
+{text:'Files',url:'files.html',children:[
+{text:'File List',url:'files.html'},
+{text:'Globals',url:'globals.html',children:[
+{text:'All',url:'globals.html',children:[
+{text:'e',url:'globals.html#index_e'},
+{text:'i',url:'globals.html#index_i'},
+{text:'k',url:'globals.html#index_k'},
+{text:'p',url:'globals.html#index_p'},
+{text:'t',url:'globals.html#index_t'},
+{text:'v',url:'globals.html#index_v'}]},
+{text:'Functions',url:'globals_func.html',children:[
+{text:'e',url:'globals_func.html#index_e'},
+{text:'p',url:'globals_func.html#index_p'},
+{text:'v',url:'globals_func.html#index_v'}]},
+{text:'Variables',url:'globals_vars.html'},
+{text:'Typedefs',url:'globals_type.html'},
+{text:'Enumerations',url:'globals_enum.html'}]}]}]}
