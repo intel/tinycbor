intel/tinycbor

commit 0170ebdcdc8886faa54f43351ff639117de98533

Fix the docs script when operating with a tag, in Travis
Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

scripts/update-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
-case "$TRAVIS_BRANCH,$TRAVIS_PULL_REQUEST" in
-    dev,false|master,false)
+case "$TRAVIS_BRANCH${TRAVIS_TAG+tag:$TRAVIS_TAG},$TRAVIS_PULL_REQUEST" in
+    dev,false|master,false|tag:*)
         ;;
     *)
         exit 0

commit c9059d9e33bfd5aff7a903c756427282f9544b61

Print the diff of the library sizes in Travis
So we can tell from the output if the library is increasing or not.

This also fixes a silly bug in testing non-tags, introduced by
0170ebdcdc8886faa54f43351ff639117de98533.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

scripts/update-docs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ex
-case "$TRAVIS_BRANCH${TRAVIS_TAG+tag:$TRAVIS_TAG},$TRAVIS_PULL_REQUEST" in
+tuple="$TRAVIS_BRANCH${TRAVIS_TAG:+tag:$TRAVIS_TAG},$TRAVIS_PULL_REQUEST"
+case "$tuple" in
     dev,false|master,false|tag:*)
         ;;
     *)
@@ -37,6 +38,7 @@ mv sizes "library_sizes/$TRAVIS_BRANCH/$QMAKESPEC"
 (cd "library_sizes/$TRAVIS_BRANCH/";
  for f in *; do echo "$f:"; cat  "$f" ; done) > "$V/library_sizes.txt"
 git add "library_sizes/$TRAVIS_BRANCH" "$V/library_sizes.txt"
+git diff --cached -U0 "$V/library_sizes.txt"
 
 # Commit everything
 if git commit -m "Update docs for $V (Travis build $TRAVIS_BUILD_NUMBER)