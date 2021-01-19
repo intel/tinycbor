35f64f343e9cc8fe405ae8292a39ebe4e4d1a853
Michael Richardson <mcr@sandelman.ca>
2021-01-19 17:07:24 -0500

    it is unclear what the return value of iterate_memcpy should be. Probably just true?

--- src/cborparser.c
+++ src/cborparser.c
@@ -1093,7 +1093,7 @@ static uintptr_t iterate_memcmp(char *s1, const uint8_t *s2, size_t len)
 
 static uintptr_t iterate_memcpy(char *dest, const uint8_t *src, size_t len)
 {
-    return (uintptr_t)memcpy(dest, src, len);
+    return (uintptr_t)(char *)memcpy(dest, src, len);
 }
 
 static CborError iterate_string_chunks(const CborValue *value, char *buffer, size_t *buflen,
